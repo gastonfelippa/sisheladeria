4ba25e5b57b67ba15d5c5522f8e5d1c9493b0124
gaston_felippa@hotmail.com <gaston_felippa@hotmail.com>
2020-12-28 17:32:28 -0300

    rrdr

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -136,7 +136,8 @@
                             <div class="">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layout"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="3" y1="9" x2="21" y2="9"></line><line x1="9" y1="21" x2="9" y2="9"></line></svg>
                                 <span>ABM</span>
-                            </div>                           
+                            </div>  
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>                      
                         </a>
                         <ul class="collapse submenu list-unstyled" id="components" data-parent="#topAccordion">
                         @can('Productos_index')
