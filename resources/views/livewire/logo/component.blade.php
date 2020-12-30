eb0b59a28b346c456416700e4cd5cf227f34b445
gaston_felippa@hotmail.com <gaston_felippa@hotmail.com>
2020-12-29 22:31:32 -0300

    gt

--- resources/views/livewire/logo/component.blade.php
+++ resources/views/livewire/logo/component.blade.php
@@ -1,6 +1,5 @@
 <div>
     <a class="navbar-brand" href="#">
-    <!-- <img src="images/logo/{{$logo}}" height="30"> -->
-    <img src={{$logo}} height="30">
+    <img src="images/logo/{{$logo}}" height="30">
     <span class="navbar-brand-name ml-2">{{$nombre}}</span></a>
 </div>
